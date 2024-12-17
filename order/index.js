7de64da6db12a5167681896764b0e82edef1f9d1
mohiandipta <mohianmostofa@gmail.com>
2024-12-17 23:42:28 +0600

    order added

--- order/index.js
+++ order/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const productRouter = require('./routes/routes')
 const { default: mongoose } = require('mongoose')
 
 const app = express()
@@ -9,19 +8,18 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.use('/product', productRouter)
 
-mongoose.connect('mongodb://mohian:mohian12345@localhost:27017/product-db', {
+mongoose.connect('mongodb://mohian:mohian12345@localhost:27017/order-db', {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 })
 .then(() => {
-    console.log('product db connected')
+    console.log('order db connected')
 })
 .catch((error) => {
     console.log(error)
 })
 
-app.listen(8001, () => {
-    console.log("Product listening to Port: 8002")
+app.listen(8002, () => {
+    console.log("Order listening to Port: 8002")
 })
